e6a809232c627be868481ff6a4add39eba3490f7
murat <muratnysambaev93@gmail.com>
2024-02-24 12:49:05 +0600

    test working

--- internal/service/chat/tests/create_test.go
+++ internal/service/chat/tests/create_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"context"
+	"fmt"
 	"github.com/Murat993/chat-server/internal/client/db"
 	txMock "github.com/Murat993/chat-server/internal/client/db/mocks"
 	"github.com/Murat993/chat-server/internal/dto"
@@ -18,12 +19,11 @@ import (
 func TestCreate(t *testing.T) {
 	t.Parallel()
 	type chatRepositoryMockFunc func(mc *minimock.Controller) repository.ChatRepository
-	type chatTxManagerMockFunc func(mc *minimock.Controller, handler db.Handler) db.TxManager
+	type chatTxManagerMockFunc func(mc *minimock.Controller) db.TxManager
 
 	type args struct {
-		ctx     context.Context
-		req     *dto.Chat
-		handler db.Handler
+		ctx context.Context
+		req *dto.Chat
 	}
 
 	var (
@@ -34,16 +34,13 @@ func TestCreate(t *testing.T) {
 		title   = gofakeit.Animal()
 		content = gofakeit.Animal()
 
-		//repoErr = fmt.Errorf("repo error")
+		repoErr = fmt.Errorf("repo error")
 
 		req = &dto.Chat{
 			Usernames: []string{title, content},
 		}
-
-		handler = func(ctx context.Context) error {
-			return nil
-		}
 	)
+
 	defer t.Cleanup(mc.Finish)
 
 	tests := []struct {
@@ -57,20 +54,42 @@ func TestCreate(t *testing.T) {
 		{
 			name: "success case",
 			args: args{
-				ctx:     ctx,
-				req:     req,
-				handler: handler,
+				ctx: ctx,
+				req: req,
 			},
 			want: id,
 			err:  nil,
-			txManagerMock: func(mc *minimock.Controller, handler db.Handler) db.TxManager {
+			txManagerMock: func(mc *minimock.Controller) db.TxManager {
 				mock := txMock.NewTxManagerMock(mc)
-				mock.ReadCommittedMock.Expect(ctx, handler).Return(nil)
+				mock.ReadCommittedMock.Set(func(ctx context.Context, f db.Handler) (err error) {
+					return f(ctx)
+				})
 				return mock
 			},
 			chatRepositoryMock: func(mc *minimock.Controller) repository.ChatRepository {
 				mock := mocks.NewChatRepositoryMock(mc)
 				mock.CreateMock.Expect(ctx, req).Return(id, nil)
+				return mock
+			},
+		},
+		{
+			name: "service error case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: 0,
+			err:  repoErr,
+			txManagerMock: func(mc *minimock.Controller) db.TxManager {
+				mock := txMock.NewTxManagerMock(mc)
+				mock.ReadCommittedMock.Set(func(ctx context.Context, f db.Handler) (err error) {
+					return f(ctx)
+				})
+				return mock
+			},
+			chatRepositoryMock: func(mc *minimock.Controller) repository.ChatRepository {
+				mock := mocks.NewChatRepositoryMock(mc)
+				mock.CreateMock.Expect(ctx, req).Return(0, repoErr)
 				return mock
 			},
 		},
@@ -82,7 +101,7 @@ func TestCreate(t *testing.T) {
 			t.Parallel()
 
 			chatRepoMock := tt.chatRepositoryMock(mc)
-			txManagerMock := tt.txManagerMock(mc, handler)
+			txManagerMock := tt.txManagerMock(mc)
 
 			service := chat.NewMockService(chatRepoMock, txManagerMock)
 
